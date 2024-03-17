185b1a863db109410fa9fe111d79e2a796010130
anaplopes <anapalopes0103@gmail.com>
2024-03-17 14:32:40 -0300

    adicionado validação de CPF no momento do signup do usuario, assim evitamos cpf invalidos

--- pre-signup/app.ts
+++ pre-signup/app.ts
@@ -1,15 +1,28 @@
-import { PreSignUpTriggerEvent } from 'aws-lambda';
+import { validate } from './validateCPF';
+import { PreSignUpTriggerEvent, Context, Callback } from 'aws-lambda';
 
-export const lambdaHandler = async (event: PreSignUpTriggerEvent): Promise<PreSignUpTriggerEvent> => {
+export const lambdaHandler = async (
+    event: PreSignUpTriggerEvent,
+    context: Context,
+    callback: Callback,
+): Promise<PreSignUpTriggerEvent> => {
+    // Impose a condition that CPF must be valid.
+    const cpf = event.userName;
+    if (!validate(cpf)) {
+        const error = new Error('CPF inválido');
+        // Return error to Amazon Cognito
+        callback(error, event);
+    }
+
     // Confirm the user
     event.response.autoConfirmUser = true;
     // Set the email as verified if it is in the request
-    if (event.request.userAttributes.hasOwnProperty("email")) {
+    if (event.request.userAttributes.hasOwnProperty('email')) {
         event.response.autoVerifyEmail = true;
     }
 
     // Set the phone number as verified if it is in the request
-    if (event.request.userAttributes.hasOwnProperty("phone_number")) {
+    if (event.request.userAttributes.hasOwnProperty('phone_number')) {
         event.response.autoVerifyPhone = true;
     }
 
--- pre-signup/tests/unit/test-handler.test.ts
+++ pre-signup/tests/unit/test-handler.test.ts
@@ -1,13 +1,42 @@
-import { PreSignUpTriggerEvent } from 'aws-lambda';
+import { PreSignUpTriggerEvent, Context, Callback } from 'aws-lambda';
 import { lambdaHandler } from '../../app';
-import { expect, describe, it } from '@jest/globals';
+import { expect, describe, it, jest, beforeEach, afterEach } from '@jest/globals';
 
 describe('Unit test for app handler', function () {
-    it('verifies successful response', async () => {
-        const event: PreSignUpTriggerEvent = JSON.parse("{\"version\":\"1\",\"region\":\"us-east-1\",\"userPoolId\":\"us-east-1_CWeZ7Rcv2\",\"userName\":\"e079eebd-cc35-4ce1-8f3e-4bdf504ad923\",\"callerContext\":{\"awsSdkVersion\":\"aws-sdk-unknown-unknown\",\"clientId\":\"2ni3m77vqrslh3ioif6cjoebs0\"},\"triggerSource\":\"PreSignUp_SignUp\",\"request\":{\"userAttributes\":{\"email\":\"userpooltester@gmail.com\"},\"validationData\":null},\"response\":{\"autoConfirmUser\":false,\"autoVerifyEmail\":false,\"autoVerifyPhone\":false}}");
-        const response: PreSignUpTriggerEvent = JSON.parse("{\"version\":\"1\",\"region\":\"us-east-1\",\"userPoolId\":\"us-east-1_CWeZ7Rcv2\",\"userName\":\"e079eebd-cc35-4ce1-8f3e-4bdf504ad923\",\"callerContext\":{\"awsSdkVersion\":\"aws-sdk-unknown-unknown\",\"clientId\":\"2ni3m77vqrslh3ioif6cjoebs0\"},\"triggerSource\":\"PreSignUp_SignUp\",\"request\":{\"userAttributes\":{\"email\":\"userpooltester@gmail.com\"},\"validationData\":null},\"response\":{\"autoConfirmUser\":true,\"autoVerifyEmail\":true,\"autoVerifyPhone\":false}}");
+    let context: Context;
+    let callback: Callback;
 
-        const result: PreSignUpTriggerEvent = await lambdaHandler(event);
+    beforeEach(async () => {
+        context = {} as Context;
+        callback = jest.fn();
+    });
+
+    afterEach(async () => {
+        jest.clearAllMocks();
+    });
+
+    it('verifies successful response', async () => {
+        const event: PreSignUpTriggerEvent = JSON.parse(
+            '{"version":"1","region":"us-east-1","userPoolId":"us-east-1_CWeZ7Rcv2","userName":"00000000191","callerContext":{"awsSdkVersion":"aws-sdk-unknown-unknown","clientId":"2ni3m77vqrslh3ioif6cjoebs0"},"triggerSource":"PreSignUp_SignUp","request":{"userAttributes":{"email":"userpooltester@gmail.com"},"validationData":null},"response":{"autoConfirmUser":false,"autoVerifyEmail":false,"autoVerifyPhone":false}}',
+        );
+        const response: PreSignUpTriggerEvent = JSON.parse(
+            '{"version":"1","region":"us-east-1","userPoolId":"us-east-1_CWeZ7Rcv2","userName":"00000000191","callerContext":{"awsSdkVersion":"aws-sdk-unknown-unknown","clientId":"2ni3m77vqrslh3ioif6cjoebs0"},"triggerSource":"PreSignUp_SignUp","request":{"userAttributes":{"email":"userpooltester@gmail.com"},"validationData":null},"response":{"autoConfirmUser":true,"autoVerifyEmail":true,"autoVerifyPhone":false}}',
+        );
+
+        const result: PreSignUpTriggerEvent = await lambdaHandler(event, context, callback);
+
+        expect(result).toEqual(response);
+    });
+
+    it('verifies error response', async () => {
+        const event: PreSignUpTriggerEvent = JSON.parse(
+            '{"version":"1","region":"us-east-1","userPoolId":"us-east-1_CWeZ7Rcv2","userName":"0000000019","callerContext":{"awsSdkVersion":"aws-sdk-unknown-unknown","clientId":"2ni3m77vqrslh3ioif6cjoebs0"},"triggerSource":"PreSignUp_SignUp","request":{"userAttributes":{"email":"userpooltester@gmail.com"},"validationData":null},"response":{"autoConfirmUser":false,"autoVerifyEmail":false,"autoVerifyPhone":false}}',
+        );
+        const response: PreSignUpTriggerEvent = JSON.parse(
+            '{"version":"1","region":"us-east-1","userPoolId":"us-east-1_CWeZ7Rcv2","userName":"0000000019","callerContext":{"awsSdkVersion":"aws-sdk-unknown-unknown","clientId":"2ni3m77vqrslh3ioif6cjoebs0"},"triggerSource":"PreSignUp_SignUp","request":{"userAttributes":{"email":"userpooltester@gmail.com"},"validationData":null},"response":{"autoConfirmUser":true,"autoVerifyEmail":true,"autoVerifyPhone":false}}',
+        );
+
+        const result: PreSignUpTriggerEvent = await lambdaHandler(event, context, callback);
 
         expect(result).toEqual(response);
     });
