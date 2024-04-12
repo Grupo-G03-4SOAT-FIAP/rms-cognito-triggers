6d819edb56de582e998a9f151adb6def968c081b
Danilo das Neves Dantas <5115895+dannevesdantas@users.noreply.github.com>
2024-04-12 15:14:21 -0300

    test: Correção nos testes unitários

--- create-auth-challenge/tests/unit/test-handler.test.ts
+++ create-auth-challenge/tests/unit/test-handler.test.ts
@@ -4,9 +4,66 @@ import { expect, describe, it } from '@jest/globals';
 
 describe('Unit test for app handler', function () {
     it('verifies successful response', async () => {
-        const event: CreateAuthChallengeTriggerEvent = JSON.parse("{\"version\":\"1\",\"region\":\"us-east-1\",\"userPoolId\":\"us-east-1_CWeZ7Rcv2\",\"userName\":\"9f64d4fb-5880-47f8-8c2d-41c644b567d0\",\"callerContext\":{\"awsSdkVersion\":\"aws-sdk-unknown-unknown\",\"clientId\":\"2ni3m77vqrslh3ioif6cjoebs0\"},\"triggerSource\":\"CreateAuthChallenge_Authentication\",\"request\":{\"userAttributes\":{\"sub\":\"9f64d4fb-5880-47f8-8c2d-41c644b567d0\",\"cognito:email_alias\":\"userpooltester@gmail.com\",\"cognito:user_status\":\"CONFIRMED\",\"email_verified\":\"true\",\"email\":\"userpooltester@gmail.com\"},\"challengeName\":\"CUSTOM_CHALLENGE\",\"session\":[],\"userNotFound\":false},\"response\":{\"publicChallengeParameters\":null,\"privateChallengeParameters\":null,\"challengeMetadata\":null}}");
-        const response: CreateAuthChallengeTriggerEvent = JSON.parse("{\"version\":\"1\",\"region\":\"us-east-1\",\"userPoolId\":\"us-east-1_CWeZ7Rcv2\",\"userName\":\"9f64d4fb-5880-47f8-8c2d-41c644b567d0\",\"callerContext\":{\"awsSdkVersion\":\"aws-sdk-unknown-unknown\",\"clientId\":\"2ni3m77vqrslh3ioif6cjoebs0\"},\"triggerSource\":\"CreateAuthChallenge_Authentication\",\"request\":{\"userAttributes\":{\"sub\":\"9f64d4fb-5880-47f8-8c2d-41c644b567d0\",\"cognito:email_alias\":\"userpooltester@gmail.com\",\"cognito:user_status\":\"CONFIRMED\",\"email_verified\":\"true\",\"email\":\"userpooltester@gmail.com\"},\"challengeName\":\"CUSTOM_CHALLENGE\",\"session\":[],\"userNotFound\":false},\"response\":{\"publicChallengeParameters\":null,\"privateChallengeParameters\":{\"challenge\":\"answer\"},\"challengeMetadata\":null}}");
-        
+        const eventJson = {
+            "version": "1",
+            "region": "us-east-1",
+            "userPoolId": "us-east-1_aX9fo84QN",
+            "userName": "00000000191",
+            "callerContext": {
+                "awsSdkVersion": "aws-sdk-unknown-unknown",
+                "clientId": "4e6phhh81h477b54ns9vfc5ahv"
+            },
+            "triggerSource": "CreateAuthChallenge_Authentication",
+            "request": {
+                "userAttributes": {
+                    "sub": "1428f428-c001-7013-6ffe-9b0100820c85",
+                    "cognito:user_status": "FORCE_CHANGE_PASSWORD",
+                    "name": "Cliente Anônimo",
+                    "email": "cliente@anonimo.com"
+                },
+                "challengeName": "CUSTOM_CHALLENGE",
+                "session": []
+            },
+            "response": {
+                "publicChallengeParameters": null,
+                "privateChallengeParameters": null,
+                "challengeMetadata": null
+            }
+        }
+
+        const event: CreateAuthChallengeTriggerEvent = eventJson as any;
+
+        const responseJson = {
+            "version": "1",
+            "region": "us-east-1",
+            "userPoolId": "us-east-1_aX9fo84QN",
+            "userName": "00000000191",
+            "callerContext": {
+                "awsSdkVersion": "aws-sdk-unknown-unknown",
+                "clientId": "4e6phhh81h477b54ns9vfc5ahv"
+            },
+            "triggerSource": "CreateAuthChallenge_Authentication",
+            "request": {
+                "userAttributes": {
+                    "sub": "1428f428-c001-7013-6ffe-9b0100820c85",
+                    "cognito:user_status": "FORCE_CHANGE_PASSWORD",
+                    "name": "Cliente Anônimo",
+                    "email": "cliente@anonimo.com"
+                },
+                "challengeName": "CUSTOM_CHALLENGE",
+                "session": []
+            },
+            "response": {
+                "publicChallengeParameters": null,
+                "privateChallengeParameters": {
+                    "challenge": "answer"
+                },
+                "challengeMetadata": null
+            }
+        }
+
+        const response: CreateAuthChallengeTriggerEvent = responseJson as any;
+
         const result: CreateAuthChallengeTriggerEvent = await lambdaHandler(event);
 
         expect(result).toEqual(response);
--- verify-auth-challenge/tests/unit/test-handler.test.ts
+++ verify-auth-challenge/tests/unit/test-handler.test.ts
@@ -4,8 +4,63 @@ import { expect, describe, it } from '@jest/globals';
 
 describe('Unit test for app handler', function () {
     it('verifies successful response', async () => {
-        const event: VerifyAuthChallengeResponseTriggerEvent = JSON.parse("{\"version\":\"1\",\"region\":\"us-east-1\",\"userPoolId\":\"us-east-1_CWeZ7Rcv2\",\"userName\":\"9f64d4fb-5880-47f8-8c2d-41c644b567d0\",\"callerContext\":{\"awsSdkVersion\":\"aws-sdk-unknown-unknown\",\"clientId\":\"2ni3m77vqrslh3ioif6cjoebs0\"},\"triggerSource\":\"VerifyAuthChallengeResponse_Authentication\",\"request\":{\"userAttributes\":{\"sub\":\"9f64d4fb-5880-47f8-8c2d-41c644b567d0\",\"cognito:email_alias\":\"userpooltester@gmail.com\",\"cognito:user_status\":\"CONFIRMED\",\"email_verified\":\"true\",\"email\":\"userpooltester@gmail.com\"},\"privateChallengeParameters\":{\"challenge\":\"answer\"},\"challengeAnswer\":\"answer\",\"userNotFound\":false},\"response\":{\"answerCorrect\":null}}");
-        const response: VerifyAuthChallengeResponseTriggerEvent = JSON.parse("{\"version\":\"1\",\"region\":\"us-east-1\",\"userPoolId\":\"us-east-1_CWeZ7Rcv2\",\"userName\":\"9f64d4fb-5880-47f8-8c2d-41c644b567d0\",\"callerContext\":{\"awsSdkVersion\":\"aws-sdk-unknown-unknown\",\"clientId\":\"2ni3m77vqrslh3ioif6cjoebs0\"},\"triggerSource\":\"VerifyAuthChallengeResponse_Authentication\",\"request\":{\"userAttributes\":{\"sub\":\"9f64d4fb-5880-47f8-8c2d-41c644b567d0\",\"cognito:email_alias\":\"userpooltester@gmail.com\",\"cognito:user_status\":\"CONFIRMED\",\"email_verified\":\"true\",\"email\":\"userpooltester@gmail.com\"},\"privateChallengeParameters\":{\"challenge\":\"answer\"},\"challengeAnswer\":\"answer\",\"userNotFound\":false},\"response\":{\"answerCorrect\":true}}");
+        const eventJson = {
+            "version": "1",
+            "region": "us-east-1",
+            "userPoolId": "us-east-1_aX9fo84QN",
+            "userName": "00000000191",
+            "callerContext": {
+                "awsSdkVersion": "aws-sdk-unknown-unknown",
+                "clientId": "4e6phhh81h477b54ns9vfc5ahv"
+            },
+            "triggerSource": "VerifyAuthChallengeResponse_Authentication",
+            "request": {
+                "userAttributes": {
+                    "sub": "1428f428-c001-7013-6ffe-9b0100820c85",
+                    "cognito:user_status": "FORCE_CHANGE_PASSWORD",
+                    "name": "Cliente Anônimo",
+                    "email": "cliente@anonimo.com"
+                },
+                "privateChallengeParameters": {
+                    "challenge": "answer"
+                },
+                "challengeAnswer": "answer"
+            },
+            "response": {
+                "answerCorrect": null
+            }
+        }
+
+        const event: VerifyAuthChallengeResponseTriggerEvent = eventJson as any;
+
+        const responseJson = {
+            "version": "1",
+            "region": "us-east-1",
+            "userPoolId": "us-east-1_aX9fo84QN",
+            "userName": "00000000191",
+            "callerContext": {
+                "awsSdkVersion": "aws-sdk-unknown-unknown",
+                "clientId": "4e6phhh81h477b54ns9vfc5ahv"
+            },
+            "triggerSource": "VerifyAuthChallengeResponse_Authentication",
+            "request": {
+                "userAttributes": {
+                    "sub": "1428f428-c001-7013-6ffe-9b0100820c85",
+                    "cognito:user_status": "FORCE_CHANGE_PASSWORD",
+                    "name": "Cliente Anônimo",
+                    "email": "cliente@anonimo.com"
+                },
+                "privateChallengeParameters": {
+                    "challenge": "answer"
+                },
+                "challengeAnswer": "answer"
+            },
+            "response": {
+                "answerCorrect": true
+            }
+        }
+
+        const response: VerifyAuthChallengeResponseTriggerEvent = responseJson as any;
 
         const result: VerifyAuthChallengeResponseTriggerEvent = await lambdaHandler(event);
 
