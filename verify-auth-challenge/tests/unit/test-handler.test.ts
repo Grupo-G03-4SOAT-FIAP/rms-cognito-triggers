e08f3aff5cb4faea8b68c60dd6e77f441cd21531
Danilo das Neves Dantas <5115895+dannevesdantas@users.noreply.github.com>
2024-04-12 15:34:19 -0300

    test: Adiciona cenário te teste

--- verify-auth-challenge/tests/unit/test-handler.test.ts
+++ verify-auth-challenge/tests/unit/test-handler.test.ts
@@ -66,4 +66,41 @@ describe('Unit test for app handler', function () {
 
         expect(result).toEqual(response);
     });
+
+    it('verifies error response', async () => {
+        const eventJson = {
+            "version": "1",
+            "region": "us-east-1",
+            "userPoolId": "us-east-1_aX9fo84QN",
+            "userName": "00000000191",
+            "callerContext": {
+                "awsSdkVersion": "aws-sdk-unknown-unknown",
+                "clientId": "4e6phhh81h477b54ns9vfc5ahv"
+            },
+            "triggerSource": "VerifyAuthChallengeResponse_Authentication",
+            "request": {
+                "userAttributes": {
+                    "sub": "1428f428-c001-7013-6ffe-9b0100820c85",
+                    "cognito:user_status": "FORCE_CHANGE_PASSWORD",
+                    "name": "Cliente Anônimo",
+                    "email": "cliente@anonimo.com"
+                },
+                "privateChallengeParameters": {
+                    "challenge": "answer"
+                },
+                "challengeAnswer": "wrong answer!"
+            },
+            "response": {
+                "answerCorrect": null
+            }
+        }
+
+        const event: VerifyAuthChallengeResponseTriggerEvent = eventJson as any;
+
+        const result: VerifyAuthChallengeResponseTriggerEvent = await lambdaHandler(event);
+
+        console.log(JSON.stringify(result));
+
+        expect(result.response.answerCorrect).toEqual(false);
+    });
 });
